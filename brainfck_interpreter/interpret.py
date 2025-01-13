6ba0fd8975e941482b1b8334090e3e934c327270
Hendrik Scherner <h.scherner@appliedai.de>
2025-01-13 15:20:15 +0100

    Add test for hello

---
+++ brainfck_interpreter/interpret.py
@@ -0,0 +1,17 @@
+"""Implementation of the brainf*ck interpreter."""
+
+
+def interpret_command(bfk_cmd: str) -> str:
+    """Interpret the brainf*ck command sequence.
+
+    Parameters
+    ----------
+    bfk_cmd : str
+        Brainf*ck command sequence.
+
+    Returns
+    -------
+    str
+        Interpretation of the command sequence (sys.out).
+    """
+    return ""
--- tests/test_interpret.py
+++ tests/test_interpret.py
@@ -1,2 +1,10 @@
-def test_import_interpret_module():
-    from brainfck_interpreter import interpret
+from brainfck_interpreter.interpret import interpret_command
+
+
+def test_interpret_returns_hello_world():
+    """Interpreter returns `Hello!` correctly."""
+    cmd_sequence = (
+        "++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>++.>+.+++++++..+++.<<+++."
+    )
+    expected_result = "Hello!"
+    assert interpret_command(cmd_sequence) == expected_result
